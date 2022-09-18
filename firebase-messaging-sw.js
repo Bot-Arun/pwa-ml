378f8ef1d4e9f066b10b9f8e3bb163dccc5cb3c9
Arun <arunmugunthan07@gmail.com>
2022-09-19 03:06:48 +0530

    initial commit

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -30,3 +30,16 @@ messaging.onBackgroundMessage((payload) => {
     self.registration.showNotification(notificationTitle,
       notificationOptions);
   });
+
+  messaging.onMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+      body: 'Background Message body.',
+      icon: '/firebase-logo.png'
+    };
+  
+    self.registration.showNotification(notificationTitle,
+      notificationOptions);
+  });
